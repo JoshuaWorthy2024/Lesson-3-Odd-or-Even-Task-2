643eb9cb4e0ec61f8d4c41736848d7e97645677e
JoshuaWorthy2024 <joshuaworthy@cgspectrum.institute>
2025-02-02 03:12:17 -0600

    Add project files.

---
+++ Lesson 3 Odd or Even Task 2/Main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using std::endl;
+using std::cout;
+using std::cin;
+
+/*
+Practice Task 2: Odd or Even
+Objective: Explore random number generation and parameter handling.
+
+Instructions:
+
+Create a program that:
+Generates a random whole number between 1 and 100 using a function.
+Prints the result.
+Determines whether the number is odd or even using another function.
+Outputs "ODD" or "EVEN" based on the result.
+Use the modulus operator (%) to check for odd/even numbers.
+
+https://www.cprogramming.com/tutorial/modulus.html
+https://www.w3schools.com/cpp/cpp_howto_random_number.asp
+*/
+
+int main() {
+
+}
